ba8f03db8b8e1a31bbcae8d51a4d4db0ff5f8231
yadavrobin <yadavrobin405@gmail.com>
2025-10-26 22:40:29 +0530

    create blogs ui done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import HomePageHeader from './components/HomePageHeader.jsx'
 import HomePage from './pages/HomePage.jsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import ProfilePage from './pages/ProfilePage.jsx';
-
+import CreatePostPage from './pages/CreatePost.jsx';
 function App() {
 	return(
 		// <HeroPage></HeroPage>
@@ -17,6 +17,7 @@ function App() {
           <Route path="/home" element={<HomePage />} />
           {/* <Route path="/publish" element={<Publish />} /> */}
           <Route path="/profile" element={<ProfilePage />} />
+          <Route path="/create-post" element={<CreatePostPage />} />
         </Routes>
       </BrowserRouter>
 		</div>
